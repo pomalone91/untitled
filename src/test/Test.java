2c0679cbac98c0ba277523806cd0c8d41c85ceeb
pomalone91 <pomalone91@gmail.com>
2018-03-31 19:42:20 -0600

    Just testing github

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("Programming is alright I guess.");
-        Sysout.out.println("pull this");
+        System.out.println("pull this");
     }
 }
